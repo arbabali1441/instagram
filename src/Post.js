9adebe8d9ed571db9ecc515fe8ae9704240aa896
Arbab Ali <arbabali1441@gmail.com>
2020-09-26 00:32:36 +0500

    completed till 37.27 react-instagram clone

---
+++ src/Post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
+
+function Post() {
+  return (
+    <div className="post">
+      <div className="post__header">
+      <Avatar
+        className="post__avatar"
+        alt='RafehQazi'
+        src="/static/images/avatar/1.jpg"
+      />
+      <h3>Username</h3>
+      </div>
+      {/* header -> avatar + username */}
+
+      <img className="post__image" src="https://hackernoon.com/images/z2xg2bpo.jpg" alt="" />
+      {/* image */}
+      
+      <h4 className="post__text"><strong>arbabali</strong> WOW day three of live session</h4>
+      {/* username + caption */}
+    </div>
+  )
+}
+
+export default Post
